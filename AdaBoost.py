56c4e92a9138ce806e4f6634c4c71e88cf44d2fe
Luca Toscano <luca.toscano4@studio.unibo.it>
2022-01-22 18:25:56 +0100

    In AdaBoost.py write the class containing the algorithm for the classification

--- AdaBoost.py
+++ AdaBoost.py
@@ -29,13 +29,81 @@ class DecisionStump:
         #predictions array is default equal to 1, with the size of the sample
         predictions = np.ones(number_of_samples)
         if self.polarity == 1:
-            #if feature is smaller then threshold prediciton is -1
+            #if sample of column X is smaller then threshold, prediciton is -1
             predictions[X_column < self.threshold] = -1
         else:
             predictions[X_column > self.threshold] = -1
             return predictions
         
-
+class Adaboost_algorithm:
+    
+    def __init__(self, n_of_classifier = 5):
+        self.n_of_classifier_ = n_of_classifier
+    
+    #the method fit need has arguments the training sample X, and the labels y
+    def fit(self, X, y):
+        #first thing is get the shape of the sample
+        n_of_samples, n_of_features= X.shape
+        
+        #initialize the weight using method full. The size of the array is equal
+        #to the size of the training sample
+        #the initial value is equal to 1/number of samples
+        weight = np.full(n_of_samples, (1/n_of_samples))
+        
+        #create a list to store all the classifier (decision stumps)
+        self.classifiers = []
+        for _ in range(self.n_of_calssifier):
+            classifier = DecisionStump()
+            
+            #I have to find the feature and the thersold 
+            #for which this error is miminum
+            min_error = float('inf')
+            for feature_i in range(n_of_features):
+                X_column = X[:, feature_i]
+                thresholds = np.unique(X_column)
+                
+                for threshold_i in thresholds:
+                    pol = 1
+                    predictions = np.ones(n_of_samples)
+                    predictions[X_column < threshold_i] = -1
+                    
+                    #error is equal to the sum of the weight of 
+                    #misclassified sample
+                    weight_misclassified= weight[y != predictions]
+                     
+                    error= sum(weight_misclassified)
+                    #flip the error if it's larger then 0.5
+                    if error > 0.5:
+                        error= 1-error
+                        pol = -1
+                    
+                    #we want to chek if the error is smaller then the min_error
+                    if error < min_error:
+                        min_error = error
+                        classifier.polarity = pol
+                        classifier.threshold = threshold_i
+                        classifier.feature_index = feature_i
+                        
+            #initialize the amount of say alpha
+            EPS = 1e-10
+            classifier.alpha = 0.5 * np.log( (1-error) / (error+EPS) )
+            
+            #
+            predictions = classifier.predict(X)
+            #formula for the update of the weights
+            weight *= np.exp(classifier.alpha * y * predictions)
+            #normalize the weight
+            weight /= np.sum(weight)
+            
+            #save the classifier
+            self.classifiers.append(classifier)
+            
+        def predict(self, X):
+            classifier_pred = [classifier.alhpa * classifier.predict(X) for classifier in self.classifiers]
+            y_pred = np.sum(classifier_pred, axis = 0)
+            y_pred = np.sign(y_pred)
+            return y_pred
+            
         
         
         
