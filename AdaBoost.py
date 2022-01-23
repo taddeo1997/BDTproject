bee76bac2a0c4c4079dcc1e4b4048302874153f1
Luca Toscano <luca.toscano4@studio.unibo.it>
2022-01-23 16:40:03 +0100

    Add computation of Gini coefficient in AdaBoost

--- AdaBoost.py
+++ AdaBoost.py
@@ -7,12 +7,11 @@ Created on Sat Jan 22 10:58:05 2022
 """
 import numpy as np
 
-#Create the lcass of the decision stump used in the AdaBoost algorithm
+#Create the class of the decision stump used in the AdaBoost algorithm as weak classifier 
 class DecisionStump:
     
     def __init__(self):
-        #says if the sample is classified as +1 or -1 for the given threshold
-        #if I want to flip the error I must flip the polarity
+        #says if the sample is classified as +1 or -1 for the given threshold if I want to flip the error I must flip the polarity
         self.polarity = 1
         self.feature_index = None
         #threshold of the feature, for the decision of the stump
@@ -39,7 +38,8 @@ class Adaboost_algorithm:
     
     def __init__(self, n_of_classifier = 5):
         self.n_of_classifier_ = n_of_classifier
-    
+        
+
     #the method fit need has arguments the training sample X, and the labels y
     def fit(self, X, y):
         #first thing is get the shape of the sample
@@ -70,8 +70,16 @@ class Adaboost_algorithm:
                     #error is equal to the sum of the weight of 
                     #misclassified sample
                     weight_misclassified= weight[y != predictions]
-                     
                     error= sum(weight_misclassified)
+                    
+                    count_array_y=np.bincount(y)
+                    count_array_predictions=np.bincount(predictions)
+                    
+                    #Compute gini coeff
+                    Gini_neg = 1 - (count_array_y[-1]/count_array_predictions[-1])**2 (count_array_y[+1]/count_array_predictions[-1])**2
+                    Gini_pos = 1 - (count_array_y[-1]/count_array_predictions[+1])**2 (count_array_y[+1]/count_array_predictions[+1])**2
+                    classifier.Gini=(count_array_predictions[-1]/n_of_samples)*Gini_neg + (count_array_predictions[+1]/n_of_samples)*Gini_pos
+            
                     #flip the error if it's larger then 0.5
                     if error > 0.5:
                         error= 1-error
@@ -86,7 +94,7 @@ class Adaboost_algorithm:
                         
             #initialize the amount of say alpha
             EPS = 1e-10
-            classifier.alpha = 0.5 * np.log( (1-error) / (error+EPS) )
+            classifier.alpha = 0.5 * np.log( (1-min_error) / (min_error+EPS) )
             
             #
             predictions = classifier.predict(X)
